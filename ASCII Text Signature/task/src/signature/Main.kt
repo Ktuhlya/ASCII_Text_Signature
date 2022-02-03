9e6c9052a34dcfc65917adbedb7fc30a105f9cbe
Ktuhlya <97504952+Ktuhlya@users.noreply.github.com>
2022-02-03 19:50:46 +0700

    1

--- ASCII Text Signature/task/src/signature/Main.kt
+++ ASCII Text Signature/task/src/signature/Main.kt
@@ -1,6 +1,17 @@
 package signature
 
-val topLitera= mapOf<String, String>("A" to "","B" to "","C" to "","D" to "","E" to "",
+val topLitera= mapOf<String, String>("A" to "____","B" to "___","C" to "____","D" to "___",
+  "E" to "____","F" to "____","G" to "____","H" to "_  _","I" to "_","J" to "_",
+  "K" to "_  _","L" to "_","M" to "_  _","N" to "_  _","O" to "____","P" to "___",
+  "Q" to "____","R" to "____","S" to "____","T" to "___","U" to "____",
+  "W" to "_ _ _","X" to "_  _","Y" to "_   _","Z" to "___")
+
+val midLitera= mapOf<String, String>("A" to "","B" to "","C" to "","D" to "","E" to "",
+  "F" to "","G" to "","H" to "","I" to "","J" to "","K" to "","L" to "","M" to "",
+  "N" to "","O" to "","P" to "","Q" to "","R" to "","S" to "","T" to "","U" to "",
+  "W" to "","X" to "","Y" to "","Z" to "")
+
+val botLitera= mapOf<String, String>("A" to "","B" to "","C" to "","D" to "","E" to "",
   "F" to "","G" to "","H" to "","I" to "","J" to "","K" to "","L" to "","M" to "",
   "N" to "","O" to "","P" to "","Q" to "","R" to "","S" to "","T" to "","U" to "",
   "W" to "","X" to "","Y" to "","Z" to "")
