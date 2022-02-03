63de23cc8c5c8552fe20f9b14110c1587363feca
Ktuhlya <97504952+Ktuhlya@users.noreply.github.com>
2022-02-03 19:38:52 +0700

    Initial commit

---
+++ ASCII Text Signature/task/src/signature/Main.kt
@@ -0,0 +1,28 @@
+package signature
+
+val topLitera= mapOf<String, String>("A" to "","B" to "","C" to "","D" to "","E" to "",
+  "F" to "","G" to "","H" to "","I" to "","J" to "","K" to "","L" to "","M" to "",
+  "N" to "","O" to "","P" to "","Q" to "","R" to "","S" to "","T" to "","U" to "",
+  "W" to "","X" to "","Y" to "","Z" to "")
+
+
+
+fun main() {
+  val firstName = readln()
+  val status = readln()
+  val long = if (firstName.length > status.length) firstName.length else status.length
+    var topBotton = "*".
+  //  println( "*".repeat(firstName.length + 4))
+
+
+
+}
+
+
+////
+
+/*
+
+}
+
+ */
