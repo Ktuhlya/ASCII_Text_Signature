8c2873c275c8fda1df449bb219e91f2de106664f
Ktuhlya <97504952+Ktuhlya@users.noreply.github.com>
2022-02-04 14:00:20 +0700

    1

--- ASCII Text Signature/task/src/signature/Main.kt
+++ ASCII Text Signature/task/src/signature/Main.kt
@@ -18,24 +18,82 @@ val botLiteral= mapOf<String, String>("A" to "|  |","B" to "|__]","C" to "|___",
   "Q" to "|_\\|","R" to "|  \\","S" to "___]","T" to " | ","U" to "|__|","V" to " \\/ ",
   "W" to "|_|_|","X" to "_/\\_","Y" to "  |  ","Z" to " /__")
 
-
+var word = mutableListOf<MutableList<String>>()
 
 fun main() {
-  val firstName = readln()
-  val status = readln()
-  val long = if (firstName.length > status.length) firstName.length else status.length
-    var topBotton = "*".
-  //  println( "*".repeat(firstName.length + 4))
+//  val firstName = readln()
+    //val status = readln()
+    // val long = if (firstName.length > status.length) firstName.length else status.length
+    //  var topBotton = "*".
+
+    val name = readln().uppercase().toMutableList()
+
+    DrawLiteral(name.map { it.toString() } as MutableList<String>, name.size)
+
+    for (i in 0..2) {
+        println(word[i].joinToString(" "))
+
+    }
+}
+
+
+
+class DrawLiteral(string: MutableList<String>, size: Int) {
+var subListTop = mutableListOf<String>()
+var subListMid = mutableListOf<String>()
+var subListBot = mutableListOf<String>()
+    init {
+        drawTop(string, size)
+        drawMid(string, size)
+        drawBot(string, size)
+    }
+
+
+   fun buildString(string: MutableList<String>) {
+
+       word.add(0, subListTop)
+       word.add(1, subListMid)
+       word.add(2, subListBot)
+
+
+   }
+
+
+    fun drawTop(string: MutableList<String>, size: Int) {
+       for(i in 0 until size) {
+          subListTop.add(i, topLiteral.getValue(string[i]))
+       }
+        buildString(subListTop)
+    }
+
+    fun drawMid(string: MutableList<String>, size: Int) {
+        for(i in 0 until size) {
+            subListMid.add(i, midLiteral.getValue(string[i]))
+        }
+        buildString(subListMid)
+    }
+
+    fun drawBot(string: MutableList<String>, size: Int) {
+        for(i in 0 until size) {
+            subListBot.add(i, botLiteral.getValue(string[i]))
+        }
+        buildString(subListBot)
+    }
+
 
 
 
 }
+
+
 
 
 ////
 
 /*
 
-}
+word.add(0, mutableListOf(drawTop("X"), drawTop("u"), drawTop("y")))
+    word.add(1, mutableListOf(drawMid("X"), drawMid("u"), drawMid("y")))
+    word.add(2, mutableListOf(drawBot("X"), drawBot("u"), drawBot("y")))
 
  */
